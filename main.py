c21287f1e7067a686f548583e7b36942fcaecf9e
redaelemawy <redaelemawy98@gmail.com>
2025-08-15 21:19:56 +0300

    add dynamic inputs for our chain

--- main.py
+++ main.py
@@ -7,18 +7,19 @@ import argparse
 load_dotenv()
 
 # define arguments
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--task", default="write a very short story!")
+parser = argparse.ArgumentParser()
+parser.add_argument("--word", default="flower")
+parser.add_argument("--language", default="french")
 
 llm = OpenAI()
-# args = parser.parse_args();
+args = parser.parse_args();
 # Run a prompt
 question_template = PromptTemplate(input_variables=["word", "language"],
                                     template="give the translation of this word: {word} in {language} language")
 question_chain = LLMChain(llm=llm, prompt= question_template )
 result = question_chain({
-    "language": "English",
-    "word": "حمار"
+    "language": args.language,
+    "word": args.word
 }) # correct method in new API
 
 print(result)
