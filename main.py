831a0bb05c5998b3936c82f6a953e0af3f565435
redaelemawy <redaelemawy98@gmail.com>
2025-08-15 21:16:47 +0300

    add first chain

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from langchain_openai import OpenAI
 from dotenv import load_dotenv
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 import argparse
 
 load_dotenv()
 
 # define arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("--task", default="write a very short story!")
+# parser = argparse.ArgumentParser()
+# parser.add_argument("--task", default="write a very short story!")
 
 llm = OpenAI()
-args = parser.parse_args();
+# args = parser.parse_args();
 # Run a prompt
-result = llm.invoke(args.task)  # correct method in new API
+question_template = PromptTemplate(input_variables=["word", "language"],
+                                    template="give the translation of this word: {word} in {language} language")
+question_chain = LLMChain(llm=llm, prompt= question_template )
+result = question_chain({
+    "language": "English",
+    "word": "حمار"
+}) # correct method in new API
 
 print(result)
